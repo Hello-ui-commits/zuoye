e1fccccca6a587501055c075659634e95b13317f
Hello-ui-commits <1875615710@qq.com>
2020-03-04 13:51:28 +0800

    draft store shopping cart update

--- jcart-store-front/viewmodels/shopping-cart.js
+++ jcart-store-front/viewmodels/shopping-cart.js
@@ -1,18 +1,24 @@
 var app = new Vue({
     el: '#app',
     data: {
-        myShoppingCart:[]
+        myShoppingCart: []
     },
-    mounted(){
+    mounted() {
         console.log('view mounted');
-        var myShoppingCartJson=localStorage['myShoppingCartJson'];
-        this.myShoppingCart=JSON.parse(myShoppingCartJson);
+        var myShoppingCartJson = localStorage['myShoppingCartJson'];
+        this.myShoppingCart = JSON.parse(myShoppingCartJson);
     },
-    methods:{
-        handleDelete(index,row){
+    methods: {
+        handleDelete(index, row) {
             console.log('delete click');
-            this.myShoppingCart.splice(index,1);
-            localStorage['myShoppingCartJson'] =JSON.stringify(this.myShoppingCart);
+            this.myShoppingCart.splice(index, 1);
+            localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+            this.$message.success("删除购物车成功");
+        },
+        handleUpdate(index,row){
+            console.log('update click');
+            localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+            this.$message.success("修改购物车成功");
         }
     }
 })
