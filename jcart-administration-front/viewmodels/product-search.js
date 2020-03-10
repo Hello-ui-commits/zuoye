845c3ccf318e0210a9fcab0d1033815389c0b5d5
Hello-ui-commits <1875615710@qq.com>
2020-03-10 15:11:58 +0800

    商品模糊未完

--- jcart-administration-front/viewmodels/product-search.js
+++ jcart-administration-front/viewmodels/product-search.js
@@ -2,12 +2,23 @@ var app = new Vue({
     el: '#app',
     data: {
         pageInfo: '',
-        pageNum: 1
+        pageNum: 1,
+        productCode:'',
+        productCode:'',
+        price:'',
+        stockQuantity:'',
+        selectedStatus:'',
+        statuses: [
+            { value: 0, label: '下架' },
+            { value: 1, label: '上架' },
+            { value: 2, label: '待审核' }
+        ],
     },
     mounted() {
         console.log('view mounted');
+        this.pageNum=1;
         this.searchProduct();
-    },
+        },
     methods: {
         handlePageChange(val){
             console.log('page change');
@@ -17,8 +28,14 @@ var app = new Vue({
         searchProduct() {
             axios.get('/product/search', {
                 params: {
+                    productCode:this.productCode,
+                    productName:this.productName,
+                    price:this.price,
+                    stockQuantity:this.stockQuantity,
+                    status:this.selectedStatus,
                     pageNum: this.pageNum
-                }
+                   }
+                
             })
                 .then(function (response) {
                     console.log(response);
